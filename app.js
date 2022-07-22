9be5e48d24e328717c4db09b5eca375b5e13eb73
qinxxxm <soldiero3of@gmail.com>
2022-07-22 17:52:31 +0800

    hard coded pre loader

--- app.js
+++ app.js
@@ -6,26 +6,34 @@ gsap.registerPlugin(ScrollTrigger);
 // });
 
 let loader = document.querySelector('.loader');
+let html = document.querySelector('html');
 
-window.addEventListener("load", () => {
+// window.addEventListener("load", () => {
+//   loader.style.display = "none";
+// });
+
+setTimeout(() => {
   loader.style.display = "none";
-});
+  html.style.overflowY = "visible";
+}, 3000);
 
-
-
-//01_Cover
+//01_Cover, there was a lag in the animation but I didnt know how to solve it
+setTimeout(() => {
   const titlePage = lottie.loadAnimation({
-  container: document.querySelector(".government-cover-1"), // the dom element that will contain the animation
-  renderer: 'svg',
-  loop: 0,
-  autoplay: true,
-  path: "./animations/01_Cover.json", // the path to the animation json
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice'
-  }
-});
+    container: document.querySelector(".government-cover-1"), // the dom element that will contain the animation
+    renderer: 'svg',
+    loop: 0,
+    autoplay: true,
+    path: "./animations/01_Cover.json", // the path to the animation json
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  });
+  
+  titlePage.setSpeed(0.8); //1 is the current speed;
+}, 3000);
+  
 
-titlePage.setSpeed(0.8); //1 is the current speed
 
 /////LOTTIE ANIMATION FUNC
 const ScrollLottie = (obj) => { //https://github.com/chrisgannon/ScrollLottie
